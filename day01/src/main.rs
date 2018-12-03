adc078199b0912c2495c0a6ba93d2672210592a6
Alexandre Bergeron <alexbergeron@users.noreply.github.com>
2018-12-02 21:23:12 -0800

    Day 1 part 1

--- day01/src/main.rs
+++ day01/src/main.rs
@@ -1,3 +1,24 @@
+use std::env;
+use std::fs::File;
+use std::io::prelude::*;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    let filename = &args[1];
+
+    let mut f = File::open(filename).expect("file not found");
+
+    let mut contents = String::new();
+    f.read_to_string(&mut contents)
+        .expect("something went wrong reading the file");
+
+    let mut frequency: isize = 0;
+    for line in contents.split("\n") {
+        if !line.is_empty() {
+            let current: isize = line.parse().unwrap();
+            frequency += current;
+        }
+    }
+
+    println!("{}", frequency);
 }
